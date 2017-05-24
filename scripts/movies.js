acc7389584baf04afc17208fe2350ffdf235719a
Anthanh Pham Trinh <antai.pt@gmail.com>
2017-05-24 14:03:01 +0200

    Bootstrap grid applied

--- scripts/movies.js
+++ scripts/movies.js
@@ -9,7 +9,7 @@
         console.log(responses);
         var html = responses.reduce(function(total, response, index, list) {
             console.log(arguments);
-            return total + '<article draggable="true"><img src="' + response.url + '" draggable="false" alt="' +
+            return total + '<article draggable="true" class="col-xs-12 col-md-6 col-lg-3"><img src="' + response.url + '" draggable="false" alt="' +
                 response.title + '"><h3>' + response.title + '</h3><p>lorem lorem</p></article>';
         }, '');
         $('#movies div').append(html);
